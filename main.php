cda174f1c5277b9d07431202776a3732c2d86c1c
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-04 18:19:41 +0300

    feat: added global variables

--- main.php
+++ main.php
@@ -4,10 +4,17 @@ require("./libs/Smarty.class.php");
 require("db.php");
 $smarty = new Smarty();
 
-// глобальные переменный для заимодейсвия с бд
+// глобальные переменный для взаимодейсвия с бд
 $comments=R::dispense('comments');
 $posts = R::dispense('posts');
 $users = R::dispense('users');
+$feedback = R::dispense('feedback');
+$eventlog = R::dispense('eventlog');
+
+$feedback -> user_id = 'test';
+$feedback -> content = 'test';
+
+R::store($feedback);
 
 
 // Отрисовка всего содержимого страницы в index.tpl
