0dcb740274f341a8f32975ba3f33f6b73e5b80f7
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-19 22:32:56 +0300

    update: add comments module

---
+++ modules/crud_comments/add_comment.php
@@ -0,0 +1,9 @@
+<?php
+$comments->comment_content = $_POST['comment'];
+$posts = R::load('posts', $_GET['id']);
+$users = R::load('users', $_GET['user_id']);
+$posts->ownCommentsList[] = $comments;
+$users->ownCommentsList[] = $comments;
+R::storeAll([$posts, $users]);
+header('Location:main.php?action=post&id=' . $_GET['id']);
+?>
