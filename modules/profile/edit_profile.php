9056b22c7ad85af3a5894723a3432c9568c8b99b
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-20 01:25:35 +0300

    fix:  empty new password check

--- modules/profile/edit_profile.php
+++ modules/profile/edit_profile.php
@@ -3,8 +3,9 @@ $user = R::load('users', $_GET['user_id']);
 $user->username = $_POST['username'];
 $user->status = $_POST['status'];
 $user->email = $_POST['email'];
-$user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$_SESSION['user']->status = $_POST['status'];
+if ($_POST['password'] !== "") {
+    $user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+}
 R::store($user);
 header(($_GET['redirect'] === 'profile') ? 'Location:main.php?section=profile&action=profile&id=' . $_GET['user_id'] : 'Location:main.php?section=users');
 ?>
