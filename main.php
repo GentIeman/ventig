42ee7642f083e441e322521fbfed1b2338fee40e
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-19 22:43:17 +0300

    updated: using a modular system

--- main.php
+++ main.php
@@ -1,8 +1,10 @@
 <?php
 // подключение зависимостей
 require("./libs/Smarty.class.php");
-require("db.php");
 $smarty = new Smarty();
+require("./modules/db.php");
+require('./modules/admin.php');
+require('./modules/user.php');
 
 // глобальные переменный для взаимодейсвия с бд
 $comments = R::dispense('comments');
@@ -14,51 +16,60 @@ $eventlog = R::dispense('eventlog');
 if (isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'post':
-            $post = R::load('posts', $_GET['id']);
-            $smarty->assign('post', $post);
-            $smarty->assign('comments', $post->ownCommentsList);
-            $smarty->assign('isCommentEdit', isset($_GET['edit_comment']));
-            if (isset($_SESSION['user'])) {
-                $smarty->assign('user', $_SESSION['user']);
-            }
-            if (isset($_GET['edit_comment'])) {
-                $smarty->assign('comment', R::load('comments', $_GET['edit_comment']));
-            }
-            $smarty->assign('posts', $smarty->fetch('post.tpl'));
+            require('./modules/crud_posts/post.php');
             break;
         case 'add_comment':
-            $comments->comment_content = $_POST['comment'];
-            $posts = R::load('posts', $_GET['id']);
-            $users = R::load('users', $_GET['user_id']);
-            $posts->ownCommentsList[] = $comments;
-            $users->ownCommentsList[] = $comments;
-            R::storeAll([$posts, $users]);
-            header('Location:main.php?action=post&id=' . $_GET['id']);
+            require('./modules/crud_comments/add_comment.php');
             break;
         case 'save_comment':
-            $comment = R::load('comments', $_GET['comment_id']);
-            $comment->comment_content = $_POST['comment'];
-            $comment->users_id = $_GET['user_id'];
-            R::store($comment);
-            header('Location:main.php?action=post&id=' . $_GET['post_id']);
+            require('./modules/crud_comments/edit_comment.php');
             break;
         case 'delete_comment':
-            $comment = R::load('comments', $_GET['comment_id']);
-            R::trash($comment);
-            header('Location:main.php?action=post&id=' . $_GET['post_id']);
+            require('./modules/crud_comments/delete_comment.php');
+            break;
         case 'delete_post':
-            $post = R::load('posts', $_GET['post_id']);
-            R::trash($post);
-            header('Location:main.php');
+            require('./modules/crud_posts/delete_post.php');
+            break;
+        case 'classic_posts':
+            require('./modules/crud_posts/classic.php');
+            break;
+        case 'novelties':
+            require('./modules/crud_posts/novelties.php');
+            break;
+        case 'search':
+        case 'search_posts':
+            require('./modules/crud_posts/search.php');
+            break;
+        case 'profile':
+            require('./modules/profile/profile.php');
+            break;
+        case 'add_post':
+            require('./modules/crud_posts/add_post.php');
+            break;
+        case 'logout':
+            require('./modules/profile/logout.php');
+            break;
+        case 'update_post':
+            require('./modules/crud_posts/update_post.php');
+            break;
+        case 'edit_profile':
+            require('./modules/profile/edit_profile.php');
+            break;
+        case 'auth':
+            require('./modules/profile/auth.php');
+            break;
+        case 'singup':
+            require('./modules/profile/singup.php');
+            break;
+        case 'singin':
+            require('./modules/profile/singin.php');
+            break;
+        case 'users':
+            require('./modules/users.php');
+            break;
     }
 } else {
-    $posts = R::findAll('posts');
-    $smarty->assign('all_posts', $posts);
-    if (isset($_SESSION['user'])) {
-        $smarty->assign('user', $_SESSION['user']);
-    }
-    $smarty->assign('posts', $smarty->fetch('posts.tpl'));
+    require('./modules/crud_posts/all_posts.php');
 }
-
 // Отрисовка всего содержимого страницы в index.tpl
 $smarty->display('index.tpl');
