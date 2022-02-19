0054dd9bdb08e35f1243132075046acaf34ca757
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-19 22:38:16 +0300

    updated: comment editing module

---
+++ modules/crud_comments/edit_comment.php
@@ -0,0 +1,7 @@
+<?php
+$comment = R::load('comments', $_GET['comment_id']);
+$comment->comment_content = $_POST['comment'];
+$comment->users_id = $_GET['user_id'];
+R::store($comment);
+header('Location:main.php?action=post&id=' . $_GET['post_id']);
+?>
