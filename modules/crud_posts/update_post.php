67d490b878c89722884c25efb1d5a0a7f4c64e4d
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-19 22:42:01 +0300

    updated: post editing module

---
+++ modules/crud_posts/update_post.php
@@ -0,0 +1,8 @@
+<?php
+$posts = R::load('posts', $_GET['post_id']);
+$posts->post_title = $_POST['title'];
+$posts->post_description = $_POST['description'];
+$posts->post_category = $_POST['category'];
+R::store($posts);
+header("Location:main.php?action=profile&id=" . $_GET['user_id']);
+?>
