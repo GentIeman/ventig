d02ec4f827ea7ad4faccfbd2c8df5553460acc69
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2022-02-19 22:36:32 +0300

    updated: admin status check module

---
+++ modules/admin.php
@@ -0,0 +1,8 @@
+<?php
+require('user.php');
+if (isset($_SESSION['user']) && $_SESSION['user']->status === 'Admin') {
+    $smarty->assign('isAdmin', true);
+} else {
+    $smarty->assign('isAdmin', false);
+}
+?>
